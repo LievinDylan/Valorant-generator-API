968dce7041fc26d5cedef3fcc25498e4a7b2ebb3
LievinDylan <lievin.dylan.pro@gmail.com>
2023-08-08 23:22:40 +0200

    Modification des chemins

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,8 +1,10 @@
 
 const app = {
     init: function () {
+        // Initialisation de la génération d'agent
        generatorManager.generateAgent();
     }
 }
 
+// Initialisation de l'app au lancement de la page
 document.addEventListener('DOMContentLoaded', app.init);
--- Javascript/generator.js
+++ Javascript/generator.js
@@ -1,7 +1,9 @@
 const generatorManager = {
+    // Génération d'un agent au clic du bouton
     generateAgent: function() {
         const generateBtn = document.querySelector('#agent_generator_btn');
         generateBtn.addEventListener("click",(event) => {
+            // Préviens l'event par défaut
             event.preventDefault();
             agentManager.displayAgent();
             const agentContainerElement = document.querySelector('#agent_container');
