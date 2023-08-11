aa9bc7dff150363afc71a69f832076d7a37ef5f2
LievinDylan <lievin.dylan.pro@gmail.com>
2023-08-11 14:56:05 +0200

    Refacto du fichier animation

--- Javascript/animation.js
+++ Javascript/animation.js
@@ -1,11 +1,11 @@
 const animationManager = {
-  animateText: function () {
+  animateTextModel: function (className) {
     let delay = 50,
       delay_start = 0,
       contents,
       letters;
 
-    document.querySelectorAll(".animate-text").forEach(function (elem) {
+    document.querySelectorAll(className).forEach(function (elem) {
       contents = elem.textContent.trim();
       elem.textContent = "";
       letters = contents.split("");
@@ -18,136 +18,28 @@ const animationManager = {
       });
       delay_start += delay * letters.length;
     });
-
+  },
+  animateText: function () {
+    this.animateTextModel(".animate-text")
   },
   animateTextWeaponEasy: function () {
-    let delay = 50,
-      delay_start = 0,
-      contents,
-      letters;
-
-    document.querySelectorAll(".animate-text-easy").forEach(function (elem) {
-      contents = elem.textContent.trim();
-      elem.textContent = "";
-      letters = contents.split("");
-      elem.style.visibility = 'visible';
-
-      letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem.textContent += letter;
-        }, delay_start + delay * index_1);
-      });
-      delay_start += delay * letters.length;
-    });
-
+    this.animateTextModel(".animate-text-easy");
   },
   animateTextWeaponMedium: function () {
-    let delay = 50,
-      delay_start = 0,
-      contents,
-      letters;
-
-    document.querySelectorAll(".animate-text-medium").forEach(function (elem) {
-      contents = elem.textContent.trim();
-      elem.textContent = "";
-      letters = contents.split("");
-      elem.style.visibility = 'visible';
-
-      letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem.textContent += letter;
-        }, delay_start + delay * index_1);
-      });
-      delay_start += delay * letters.length;
-    });
-
+    this.animateTextModel(".animate-text-medium");
   },
   animateTextWeaponHard: function () {
-    let delay = 50,
-      delay_start = 0,
-      contents,
-      letters;
-
-    document.querySelectorAll(".animate-text-hard").forEach(function (elem) {
-      contents = elem.textContent.trim();
-      elem.textContent = "";
-      letters = contents.split("");
-      elem.style.visibility = 'visible';
-
-      letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem.textContent += letter;
-        }, delay_start + delay * index_1);
-      });
-      delay_start += delay * letters.length;
-    });
-
+    this.animateTextModel(".animate-text-hard");
   },
   animateTextSkillEasy: function () {
-    let delay = 50,
-      delay_start = 0,
-      contents,
-      letters;
-
-    document.querySelectorAll(".animate-text-skill-easy").forEach(function (elem) {
-      contents = elem.textContent.trim();
-      elem.textContent = "";
-      letters = contents.split("");
-      elem.style.visibility = 'visible';
-
-      letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem.textContent += letter;
-        }, delay_start + delay * index_1);
-      });
-      delay_start += delay * letters.length;
-    });
-
+    this.animateTextModel(".animate-text-skill-easy");
   },
   animateTextSkillMedium: function () {
-    let delay = 50,
-      delay_start = 0,
-      contents,
-      letters;
-
-    document.querySelectorAll(".animate-text-skill-medium").forEach(function (elem) {
-      contents = elem.textContent.trim();
-      elem.textContent = "";
-      letters = contents.split("");
-      elem.style.visibility = 'visible';
-
-      letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem.textContent += letter;
-        }, delay_start + delay * index_1);
-      });
-      delay_start += delay * letters.length;
-    });
-
+    this.animateTextModel(".animate-text-skill-medium");
   },
   animateTextSkillHard: function () {
-    let delay = 50,
-      delay_start = 0,
-      contents,
-      letters;
-
-    document.querySelectorAll(".animate-text-skill-hard").forEach(function (elem) {
-      contents = elem.textContent.trim();
-      elem.textContent = "";
-      letters = contents.split("");
-      elem.style.visibility = 'visible';
-
-      letters.forEach(function (letter, index_1) {
-        setTimeout(function () {
-          elem.textContent += letter;
-        }, delay_start + delay * index_1);
-      });
-      delay_start += delay * letters.length;
-    });
-
+    this.animateTextModel(".animate-text-skill-hard");
   },
-
-}
-
+};
 
 
